989009365add6e265f0ddae9a0aa0e1b4bd46fba
Piotr Bechcicki <piotr.bechcicki.o@gmail.com>
2018-12-03 20:55:57 +0100

    Basic config.

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+    entry: {
+        app: './src/app.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js',
+    },
+    devServer: {
+      contentBase: './build',
+      hot: true,
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+        }],
+    },
+}
